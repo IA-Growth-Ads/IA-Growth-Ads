fd210fd91cb155105db307272758ff3ae52078fa
IA-Growth-Ads <younousoumar90@gmail.com>
2025-05-12 01:19:04 +0100

    Create advertisement.py

---
+++ backend/models/advertisement.py
@@ -0,0 +1,9 @@
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Advertisement(Base):
+    __tablename__ = "advertisements"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    description = Column(String)
